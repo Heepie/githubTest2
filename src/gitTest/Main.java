12d3dca3f8e9f88b48f655929617a86b2dd4dc8a
heepie <gheepie@gmail.com>
2017-11-13 12:34:17 +0900

    Revert "문구 변"
    This reverts commit 0ed1d6ab200531f5547567b895eb734fc2e11954.

--- src/gitTest/Main.java
+++ src/gitTest/Main.java
@@ -5,8 +5,4 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Oh! Happy Day");
 	}
-	
-	public static void test() {
-		System.out.println("This is test method");
-	}
 }
