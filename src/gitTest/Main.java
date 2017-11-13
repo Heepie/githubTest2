0ed1d6ab200531f5547567b895eb734fc2e11954
heepie <gheepie@gmail.com>
2017-11-13 12:31:42 +0900

    문구 변

--- src/gitTest/Main.java
+++ src/gitTest/Main.java
@@ -5,4 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Oh!! Heepie Day");
 	}
+	
+	public static void test() {
+		System.out.println("This is test method");
+	}
 }
